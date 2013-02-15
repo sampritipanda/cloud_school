ba3880c9991b7aea172d89cce3f4522940a5335a
Sampriti Panda <sampdingo@hotmail.com>
2013-02-15 06:47:10 -0500

    Initialize the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudSchool::Application.config.secret_token = '2b1fe1db698eddb5ae8f831c69b1bbe5902a6da2c0534ecf3bc2c52d2028a3b242854e70dba714020a59bf63aad5682d4fb69580c9b5571d0f9402139d7104ca'
