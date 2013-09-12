4c951ef89d8642390700d3af3136ef29e477df7f
Sampriti Panda <sampdingo@hotmail.com>
2013-09-12 13:19:13 -0400

    Added ebook size and format validation

--- app/models/ebook.rb
+++ app/models/ebook.rb
@@ -4,6 +4,9 @@ class Ebook < ActiveRecord::Base
   
   attr_accessible :name, :book
   has_attached_file :book
-  
+
   validates_presence_of :name, :book
+  validates_attachment :book, :presence => true,
+    :content_type => { :content_type => "application/pdf" },
+    :size => { :in => 0..10.megabytes }
 end
